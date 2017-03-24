f58dfe066a75ed27586bac2e05f12b1e3eda42be
Nathan Stuart <nathan.stuart01@gmail.com>
2017-03-24 06:25:34 -0600

    javascript memory match game, started, not complete

---
+++ memorymatch.js
@@ -0,0 +1,21 @@
+// eventually include an array of fruits into the game
+
+$(document).ready(function() {
+
+$("#fruit1").click(function(){
+    $("#fruit1").fadeOut();
+});
+});
+
+
+
+//create a way to keep display of only matching fruits
+// if apple = apple, display matching fruits
+//  else, remove appended text
+
+// function (fruit 1, fruit 2)
+  // on click, if fruit 1 == fruit 2, display both fruits
+  // else on click , if fruit 1 != fruit 2, hide both fruits
+//create a way to hide two fruits if not matched
+//create a way to hide unmatched fruits
+//create a way to keep displaying matched fruits
